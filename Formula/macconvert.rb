0327ec0ae12062de68d17e50b5f184ca666affe1
Pedro Gimenez <pehqge@gmail.com>
2026-06-11 00:06:25 -0300

    macconvert 1.2.0

--- Formula/macconvert.rb
+++ Formula/macconvert.rb
@@ -1,8 +1,8 @@
 class Macconvert < Formula
   desc "Convert files from Finder's right-click menu - 45 native Quick Actions"
   homepage "https://github.com/pehqge/macconvert"
-  url "https://github.com/pehqge/macconvert/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "33fd148769bc71ab385006d67d039d2c73f07ac1e924c5f221fbf5617c192249"
+  url "https://github.com/pehqge/macconvert/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "934bc101c087986c9be18b095899a75e0db99c1bf7a2f8290ab98031ce6040e0"
   license "MIT"
 
   depends_on :macos
